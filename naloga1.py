77e1cad7ee1a46ec9d496c35f161a8509c9a144b
David Čadež <david.cadez7@gmail.com>
2022-06-19 20:15:36 +0200

    b

--- naloga1.py
+++ naloga1.py
@@ -23,91 +23,155 @@ print(f"Ocena za delež je {mi:0.3f}.")
 # b)
 # Standardna napaka cenilke iz naloge a)
 # Pomagaj si s primerom C na strani 209
-sigma = sample['brez_ss'].std(ddof=0)
-
-SE = sample['brez_ss'].sem(ddof=0)
-print(SE)
-SE_plus = sample['brez_ss'].sem()
-print(SE_plus)
+SE = math.sqrt((N - n) * mi * (1 - mi) / ((n - 1) * N))
 
 # https://stackoverflow.com/questions/53519823/confidence-interval-in-python-dataframe
-ci = (mi - 1.96 * sigma / math.sqrt(n), mi + 1.96 * sigma / math.sqrt(n))
-print(ci)
+CI = (mi - 1.96 * SE, mi + 1.96 * SE)
+print(f"Interval zaupanja je {CI}.")
 
 # c)
+# pop_mi = X['brez_ss'].mean()
+# pop_sigma = X['brez_ss'].std(ddof=0)
+# pop_SE = ((N - n) / (N - 1) * (pop_sigma**2) / n)**(1 / 2)
+# pop_ci = (pop_mi - 1.96 * pop_sigma / math.sqrt(N),
+#           pop_mi + 1.96 * pop_sigma / math.sqrt(N))
+# print(pop_ci)
+
 pop_mi = X['brez_ss'].mean()
-pop_sigma = X['brez_ss'].std(ddof=0)
-pop_SE = ((N - n) / (N - 1) * (pop_sigma**2) / n)**(1 / 2)
-pop_ci = (pop_mi - 1.96 * pop_sigma / math.sqrt(N),
-          pop_mi + 1.96 * pop_sigma / math.sqrt(N))
-print(pop_ci)
-if ci[0] < pop_mi < ci[1]:
-    print("Da, interval zaupanja pokrije populacijski delež: ", pop_mi)
+pop_SE = X['brez_ss'].sem()
+
+if CI[0] < pop_mi < CI[1]:
+    print(f"Da, interval zaupanja pokrije populacijski delež: {pop_mi}")
 else:
-    print("Ne, interval zaupanja ne pokrije populacijskega deleža: ", pop_mi)
+    print(f"Ne, interval zaupanja ne pokrije populacijskega deleža: {pop_mi}")
 
 # d)
-n_tests = 99
+tests = 99
 count = 0
-samples = pd.DataFrame(columns=['low', 'high', 'mi', 'sigma'])
-ci_df = pd.DataFrame([[ci[0], ci[1], mi, sigma]],
-                     columns=['low', 'high', 'mi', 'sigma'])
+samples = pd.DataFrame(columns=['low', 'high', 'mi', 'SE'])
+ci_df = pd.DataFrame([[CI[0], CI[1], mi, SE]],
+                     columns=['low', 'high', 'mi', 'SE'])
 samples = pd.concat([samples, ci_df], ignore_index=True)
-if ci[0] < pop_mi < ci[1]:
+
+if CI[0] < pop_mi < CI[1]:
     count += 1
-for i in range(n_tests):
+
+for i in range(tests):
     sample = X.sample(n)
     mi = sample['brez_ss'].mean()
-    sigma = sample['brez_ss'].std(ddof=0)
-    ci = (mi - 1.96 * sigma / math.sqrt(n), mi + 1.96 * sigma / math.sqrt(n))
-    ci_df = pd.DataFrame([[ci[0], ci[1], mi, sigma]],
-                         columns=['low', 'high', 'mi', 'sigma'])
+    SE = math.sqrt((N - n) * mi * (1 - mi) / ((n - 1) * N))
+    CI = (mi - 1.96 * SE, mi + 1.96 * SE)
+    ci_df = pd.DataFrame([[CI[0], CI[1], mi, SE]],
+                         columns=['low', 'high', 'mi', 'SE'])
     samples = pd.concat([samples, ci_df], ignore_index=True)
-    if ci[0] < pop_mi < ci[1]:
+    if CI[0] < pop_mi < CI[1]:
         count += 1
-print(f"{count} od {n_tests+1} jih pokrije populacijski delež.")
-print(samples)
+
+print(
+    f"Pri n={n} {100*count/(tests+1)}% intervalov zaupanja pokrije populacijski delež."
+)
+# print(samples)
 
 for i, low, high, _, _ in samples.to_records():
     plt.plot((low, high), (i, i), color='blue')
-plt.plot((pop_mi, pop_mi), (0, 99), color='red')
-plt.savefig("output_1.png")
+plt.plot((pop_mi, pop_mi), (0, tests), color='red')
+plt.savefig("1d.png")
+
+plt.clf()
 
 # e)
-STD_mi_samples = samples['mi'].std(ddof=0)
-print(STD_mi_samples)
-print(pop_SE)
+# Standardni odklon vzorčnih deležev za 100 prej dobljenih vzorcev
+# std_mi_samples = samples['mi'].std(ddof=0)
+# print(pop_SE)
 
 # f)
 n = 800
-n_samples = 100
-count2 = 0
-samples2 = pd.DataFrame(columns=['low', 'high', 'mi', 'sigma'])
-for _ in range(n_samples):
+tests = 100
+count = 0
+samples = pd.DataFrame(columns=['low', 'high', 'mi', 'SE'])
+
+for _ in range(tests):
     sample = X.sample(n)
     mi = sample['brez_ss'].mean()
-    sigma = sample['brez_ss'].std(ddof=0)
-    ci = (mi - 1.96 * sigma / math.sqrt(n), mi + 1.96 * sigma / math.sqrt(n))
+    SE = math.sqrt((N - n) * mi * (1 - mi) / (N * (n - 1)))
+    CI = (mi - 1.96 * SE, mi + 1.96 * SE)
 
-    ci_df = pd.DataFrame([[ci[0], ci[1], mi, sigma]],
-                         columns=['low', 'high', 'mi', 'sigma'])
-    samples2 = pd.concat([samples2, ci_df], ignore_index=True)
+    ci_df = pd.DataFrame([[CI[0], CI[1], mi, SE]],
+                         columns=['low', 'high', 'mi', 'SE'])
+    samples = pd.concat([samples, ci_df], ignore_index=True)
 
-    if ci[0] < pop_mi < ci[1]:
-        count2 += 1
+    if CI[0] < pop_mi < CI[1]:
+        count += 1
 
-for i, low, high, _, _ in samples2.to_records():
+for i, low, high, _, _ in samples.to_records():
     plt.plot((low, high), (i, i), color='blue')
-plt.plot((pop_mi, pop_mi), (0, 99), color='red')
-plt.savefig("output_2.png")
+plt.plot((pop_mi, pop_mi), (0, tests), color='red')
+plt.savefig("1f.png")
 
-print(samples2)
+plt.clf()
 
 print(
-    f"Pri n={n} {count2} od {n_samples} vzorcev pokrije populacijsko povprečje."
+    f"Pri n={n} {100*count/tests}% intervalov zaupanja pokrije populacijski delež."
 )
 
-pop_SE = ((N - n) / (N - 1) * (pop_sigma**2) / n)**(1 / 2)
-STD_mi_samples2 = samples2['mi'].std(ddof=0)
-print(STD_mi_samples2)
-print(pop_SE)
+# pop_SE = ((N - n) / (N - 1) * (pop_sigma**2) / n)**(1 / 2)
+# STD_mi_samples2 = samples['mi'].std(ddof=0)
+# print(STD_mi_samples2)
+# print(pop_SE)
+#
+
+# n = 1600
+# tests = 100
+# count = 0
+# samples = pd.DataFrame(columns=['low', 'high', 'mi', 'SE'])
+#
+# for _ in range(tests):
+#     sample = X.sample(n)
+#     mi = sample['brez_ss'].mean()
+#     SE = math.sqrt((N - n) / (N * n) * sample['brez_ss'].var())
+#     CI = (mi - 1.96 * SE, mi + 1.96 * SE)
+#
+#     ci_df = pd.DataFrame([[CI[0], CI[1], mi, SE]],
+#                          columns=['low', 'high', 'mi', 'SE'])
+#     samples = pd.concat([samples, ci_df], ignore_index=True)
+#
+#     if CI[0] < pop_mi < CI[1]:
+#         count += 1
+#
+# for i, low, high, _, _ in samples.to_records():
+#     plt.plot((low, high), (i, i), color='blue')
+# plt.plot((pop_mi, pop_mi), (0, tests-1), color='red')
+# plt.savefig("1z.png")
+#
+# plt.clf()
+#
+# print(
+#     f"Pri n={n} {100*count/tests}% intervalov zaupanja pokrije populacijski delež."
+# )
+
+# n = 3200
+# tests = 100
+# count = 0
+# samples = pd.DataFrame(columns=['low', 'high', 'mi', 'SE'])
+#
+# for _ in range(tests):
+#     sample = X.sample(n)
+#     mi = sample['brez_ss'].mean()
+#     SE = math.sqrt((N - n) / (N * n) * sample['brez_ss'].var())
+#     CI = (mi - 1.96 * SE, mi + 1.96 * SE)
+#
+#     ci_df = pd.DataFrame([[CI[0], CI[1], mi, SE]],
+#                          columns=['low', 'high', 'mi', 'SE'])
+#     samples = pd.concat([samples, ci_df], ignore_index=True)
+#
+#     if CI[0] < pop_mi < CI[1]:
+#         count += 1
+#
+# for i, low, high, _, _ in samples.to_records():
+#     plt.plot((low, high), (i, i), color='blue')
+# plt.plot((pop_mi, pop_mi), (0, tests-1), color='red')
+# plt.savefig("1ž.png")
+#
+# print(
+#     f"Pri n={n} {100*count/tests}% intervalov zaupanja pokrije populacijski delež."
+# )
