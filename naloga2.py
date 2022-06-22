a0865ac1985d8c80a0f5d27e382ef22be7a03a9c
David Čadež <david.cadez7@gmail.com>
2022-06-22 02:00:08 +0200

    p

--- naloga2.py
+++ naloga2.py
@@ -11,40 +11,37 @@ X = X.drop('DAN', axis=1)
 
 fig, plots = plt.subplots(1, 3, figsize=(10, 5), constrained_layout=True)
 
-#fig.tight_layout()
-
 plots[0].title.set_text('Histogram')
 plots[1].title.set_text('Viseči histogram')
 plots[2].title.set_text('Primerjalni kvantilni (Q-Q) grafikon')
 
-all_measurements = pd.DataFrame(pd.concat([
+Y = pd.DataFrame(pd.concat([
     X['ODLITEK1'], X['ODLITEK2'], X['ODLITEK3'], X['ODLITEK4'], X['ODLITEK5']
 ],
-                                          ignore_index=True),
-                                columns=['value'])
+                           ignore_index=True),
+                 columns=['value'])
 
 # a)
-n = len(all_measurements)
+n = len(Y)
 
-[q1, q3] = all_measurements['value'].quantile([.25, .75])
+[q1, q3] = Y['value'].quantile([.25, .75])
 IQR = q3 - q1
 
 # Using Freedman-Diaconis rule
 width = 2.6 * IQR / (n**(1 / 3))
 
+print(f"Širina intervalov izračunana po modificiranem \
+Freedman-Diaconisovem pravilu je enaka {width:0.5f}.")
+
 # Define bins
-bins = int(
-    math.ceil(
-        (all_measurements['value'].max() - all_measurements['value'].min()) /
-        width))
+bins = int(math.ceil((Y['value'].max() - Y['value'].min()) / width))
 
 # all_measurements['value'].plot.kde(ax=plots[0])
-mi = all_measurements['value'].mean()
-sigma = all_measurements['value'].std()
+mi = Y['value'].mean()
+sigma = Y['value'].std()
 
 mu, std = norm.fit(X)
-x = np.linspace(all_measurements['value'].min(),
-                all_measurements['value'].max(), 100)
+x = np.linspace(Y['value'].min(), Y['value'].max(), 100)
 p = norm.pdf(x, mu, std)
 plots[0].plot(x, p, 'k', linewidth=2)
 
@@ -57,11 +54,11 @@ def aux(x, mi, sigma):
     return phi((x + width - mi) / sigma) - phi((x - mi) / sigma)
 
 
-all_measurements['value'].hist(bins=bins, density=True, ax=plots[0])
+Y['value'].hist(bins=bins, density=True, ax=plots[0])
 
 # b)
 freq = pd.DataFrame()
-freq = round(((all_measurements['value'] // width) * width + width / 2.), 2)
+freq = round(((Y['value'] // width) * width + width / 2.), 2)
 freq = pd.DataFrame(freq.value_counts())
 freq['obs_count'] = 24 * 5 * freq / freq.sum()
 freq['exp_count'] = 24 * 5 * aux(freq.index, mi, sigma)
@@ -70,7 +67,7 @@ freq['diffs'] = freq['obs_count'] - freq['exp_count']
 freq['diffs'].plot.bar(ax=plots[1])
 
 # c)
-sm.qqplot(all_measurements['value'],
+sm.qqplot(Y['value'],
           xlabel="Normalna porazdelitev",
           ylabel="Porazdelitev meritev",
           ax=plots[2])
--- naloga3.py
+++ naloga3.py
@@ -1,12 +1,76 @@
 import math
-import statsmodels.api as sm
+import numpy as np
 import pandas as pd
-from scipy.stats import norm
 import matplotlib.pyplot as plt
 
-X = pd.read_csv("Temp_LJ.csv")
-X.columns = X.columns.str.strip("'")
+data = pd.read_csv("Temp_LJ.csv")
+data.columns = data.columns.str.strip("'")
 
-# model A
-# linearen trend in sinusno nihanje s periodo eno leto
-Y = a + b * X["LETO"] + A * math.sin((2*math.pi/12) * (X["MESEC"] - 1) + omega)
+Y = data['TEMPERATURA'].to_numpy()
+Y = Y - Y.mean()
+
+n = Y.shape[0]
+
+Y = Y.reshape([n, 1])
+
+X_A = np.arange(n).reshape([n, 1])
+sin = np.sin(math.pi * np.arange(n) / 6)
+cos = np.cos(math.pi * np.arange(n) / 6)
+sin = sin.reshape([n, 1])
+cos = cos.reshape([n, 1])
+X_A = np.concatenate((X_A, sin, cos), axis=1)
+
+# Po metodi največjega verjetja je cenilka za vektor b rešitev predoločenega
+# sistema
+beta_A = np.dot(np.dot(np.linalg.inv(np.dot(X_A.T, X_A)), X_A.T), Y)
+
+print("Vektor ocen za parametre modela A je: ",
+      list(np.around(beta_A[:, 0], 3)))
+
+# Narišemo graf
+f = plt.figure()
+f.set_figwidth(8)
+f.set_figheight(3)
+plt.plot(np.dot(X_A, beta_A)[300:] + data['TEMPERATURA'].mean(), linewidth=1.5)
+plt.plot(Y[300:] + data['TEMPERATURA'].mean(), linewidth=1.5)
+plt.xlabel("Meseci od januarja 2011")
+plt.ylabel("Temperatura v stopinjah celzija")
+plt.tight_layout()
+plt.savefig("3a.png")
+plt.clf()
+
+RSS_A = np.linalg.norm(Y - np.dot(X_A, beta_A))
+
+AIC_A = 2 * 4 + n * np.log(RSS_A)
+print(f"Akaikejeva informacija za model A: {AIC_A}")
+
+X_B = np.arange(n).reshape([n, 1])
+for i in range(12):
+    tmp = (np.arange(n) % 12) == i
+    tmp = tmp.astype(int)
+    tmp = tmp.reshape([n, 1])
+    X_B = np.concatenate((X_B, tmp), axis=1)
+
+# Po metodi največjega verjetja je cenilka za vektor b rešitev predoločenega
+# sistema
+beta_B = np.dot(np.dot(np.linalg.inv(np.dot(X_B.T, X_B)), X_B.T), Y)
+
+print("Vektor ocen za parametre modela B je: ",
+      list(np.around(beta_B[:, 0], 3)))
+
+# Narišemo graf
+f = plt.figure()
+f.set_figwidth(8)
+f.set_figheight(3)
+plt.plot(np.dot(X_B, beta_B)[300:] + data['TEMPERATURA'].mean(), linewidth=1.5)
+plt.plot(Y[300:] + data['TEMPERATURA'].mean(), linewidth=1.5)
+plt.xlabel("Meseci od januarja 2011")
+plt.ylabel("Temperatura v stopinjah celzija")
+plt.tight_layout()
+plt.savefig("3b.png")
+plt.clf()
+
+RSS_B = np.linalg.norm(Y - np.dot(X_B, beta_B))
+
+AIC_B = 2 * (1 + 12) + n * np.log(RSS_B)
+print(f"Akaikejeva informacija za model B: {AIC_B}")
